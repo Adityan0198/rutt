a50d231cb689c70f9388c0d5d21c8dc54aee2844
Adityan <152312999+Adityan0198@users.noreply.github.com>
2026-07-05 21:03:47 +0530

    Add Dummy Methods, Update Structs
    Added Consts, Pos struct for validity of positions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use crate::MiniBoardState::Play;
+const SIZE: usize = 3;
 
 #[derive(Copy, Clone, Debug)]
 enum Tile {
@@ -15,27 +15,52 @@ enum MiniBoardState {
 
 #[derive(Copy, Clone, Debug)]
 struct MiniBoard {
-    mboard: [[Option<Tile>; 3]; 3],
+    mboard: [[Option<Tile>; SIZE]; SIZE],
     state: MiniBoardState,
 }
 
 impl MiniBoard {
     fn new() -> Self {
         Self {
-            mboard: [[None; 3]; 3],
-            state: Play,
+            mboard: [[None; SIZE]; SIZE],
+            state: MiniBoardState::Play,
         }
+    }
+
+    fn update_state(&mut self) -> bool {
+        todo!()
     }
 }
 
+pub struct Pos(usize, usize);
+
 struct BigBoard {
-    board: [[MiniBoard; 3]; 3],
+    board: [[MiniBoard; SIZE]; SIZE],
+    playable: Option<Pos>, //If None, can play anywhere
+}
+
+impl Pos {
+    pub fn from(x: usize, y: usize) -> Self {
+        if x >= SIZE || y >= SIZE {
+            panic!("Position out of bounds")
+        }
+        Pos(x, y)
+    }
 }
 
 impl BigBoard {
     fn new() -> Self {
         Self {
-            board: [[MiniBoard::new(); 3]; 3],
+            board: [[MiniBoard::new(); SIZE]; SIZE],
+            playable: None,
         }
+    }
+
+    fn play(&mut self, board_pos: Pos, pos: Pos, tile: Tile) -> Result<(), i32> {
+        todo!()
+    }
+
+    fn check_win(&self) -> Tile {
+        todo!()
     }
 }
