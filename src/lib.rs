418c81b6027cda81aff6fc570afa975718485e9d
Adityan <152312999+Adityan0198@users.noreply.github.com>
2026-07-05 23:57:43 +0530

    Add update_state method for mini brd logic

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ use anyhow::{Result, anyhow};
 
 const SIZE: usize = 3;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Tile {
     X,
     O,
 }
+
+pub struct Pos(usize, usize);
 
 #[derive(Copy, Clone, Debug)]
 enum MiniBoardState {
@@ -29,22 +31,59 @@ impl MiniBoard {
         }
     }
 
-    fn play(&mut self, pos: Pos, tile: Tile) -> Result<()> {
+    fn play(&mut self, pos: Pos, tile: Tile) -> Result<MiniBoardState> {
         let Pos(x, y) = pos;
         if self.mboard[x][y].is_none() {
             self.mboard[x][y] = Some(tile);
-            Ok(())
+            Ok(self.update_state(tile))
         } else {
             Err(anyhow!("Invalid Move (Occupied)"))
         }
     }
 
-    fn update_state(&mut self) -> bool {
-        todo!()
+    fn update_state(&mut self, tile: Tile) -> MiniBoardState {
+        //Update win/tie state based on last move, returns the state
+
+        let mut fwd_diag = 0; //Diag \
+        let mut bkwd_diag = 0; //Diag /
+        let mut along_y = [0 as usize; SIZE];
+        let mut along_x = [0 as usize; SIZE];
+
+        let mut any_none = false;
+
+        for x in 0..SIZE {
+            for y in 0..SIZE {
+                let Some(entry) = self.mboard[x][y] else {
+                    any_none = true;
+                    continue;
+                };
+                if entry != tile {
+                    continue;
+                }
+                if x == y {
+                    fwd_diag += 1;
+                }
+                if x == (SIZE - 1 - y) {
+                    bkwd_diag += 1
+                }
+                along_y[x] += 1;
+                along_x[y] += 1;
+            }
+        }
+
+        if fwd_diag == SIZE
+            || bkwd_diag == SIZE
+            || along_x.contains(&SIZE)
+            || along_y.contains(&SIZE)
+        {
+            self.state = MiniBoardState::Win(tile);
+        } else if !any_none {
+            self.state = MiniBoardState::Tie;
+        }
+
+        self.state
     }
 }
-
-pub struct Pos(usize, usize);
 
 struct BigBoard {
     board: [[MiniBoard; SIZE]; SIZE],
